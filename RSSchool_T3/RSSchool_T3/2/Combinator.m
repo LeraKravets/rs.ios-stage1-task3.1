ff2890b806a077d1479a5cff752ccaab9f858c57
Lera Kravets <kravetsvaleryia@gmail.com>
2020-04-05 23:17:22 +0300

    feat: add task2 Combinator

--- RSSchool_T3/RSSchool_T3/2/Combinator.m
+++ RSSchool_T3/RSSchool_T3/2/Combinator.m
@@ -2,6 +2,54 @@
 
 @implementation Combinator
 - (NSNumber*)chechChooseFromArray:(NSArray <NSNumber*>*)array {
-    return @0;
+    NSInteger m = array.firstObject.integerValue;
+    NSInteger totalNumber = array.lastObject.integerValue;
+
+//    - (NSInteger)findFactorial:(NSInteger)a {
+//        for (int a=1; n>0; n--) {
+//            a *= n;
+//        return a;
+//        }
+//    }
+
+    for (int i=1; i<=10; i++) {
+        NSInteger factN = 1;
+        NSInteger n = totalNumber;
+        for (factN=1; n>0; n--) {
+            factN *= n;
+        }
+
+        NSInteger factK = 1;
+        NSInteger k = i;
+        for (factK=1; k>0; k--) {
+            factK *= k;
+        }
+
+        NSInteger factNk = 1;
+        NSInteger dif = totalNumber-i;
+        for (factNk=1; dif>0; dif--) {
+            factNk *= dif;
+        }
+
+        NSInteger fact = factN / (factK*factNk);
+
+//        while (fact != m) {
+//            result = false;
+//        } result = true;
+//
+//        if (result) {
+//            return k;
+//        } else {
+//            return nil;
+//        }
+        if (fact == m) {
+            NSNumber *result = @(i);
+            return result;
+        } else {
+            continue;
+        }
+    }
+    return nil;
 }
+
 @end
